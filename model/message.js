10fbe32efb48bd7adbd8cc18595b16047288b198
Emil <egrigoryan264@gmail.com>
2024-12-25 19:28:16 +0400

    add images info

--- model/message.js
+++ model/message.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
-  titlefirst: {
-    type: String,
-    required: true
-  },
-  titlesecond: {
-    type: String,
-    required: true
-  },
-  to:{
-    type: String,
-    required: true
-  },
-  name:{
-    type: String,
-    required: true
-  },
-  email:{
-    type: String,
-    required: true
-  },
-  number:{
-    type: String,
-    required: true
-  },
-  type:{
-    type: String,
-    required: true
-  }, 
-  information:{
-    type: String,
-    required: true
-  },
-  from:{
-    type: String,
-    required: true
-  }
+// const messageSchema = new mongoose.Schema({
+//   titlefirst: {
+//     type: String,
+//     required: true
+//   },
+//   titlesecond: {
+//     type: String,
+//     required: true
+//   },
+//   to:{
+//     type: String,
+//     required: true
+//   },
+//   name:{
+//     type: String,
+//     required: true
+//   },
+//   email:{
+//     type: String,
+//     required: true
+//   },
+//   number:{
+//     type: String,
+//     required: true
+//   },
+//   type:{
+//     type: String,
+//     required: true
+//   }, 
+//   information:{
+//     type: String,
+//     required: true
+//   },
+//   from:{
+//     type: String,
+//     required: true
+//   }
 
 
-});
+// });
 
-const MessageTypes = mongoose.model('Message', messageSchema);
+// const MessageTypes = mongoose.model('Message', messageSchema);
 
-module.exports = MessageTypes;
+// module.exports = MessageTypes;
